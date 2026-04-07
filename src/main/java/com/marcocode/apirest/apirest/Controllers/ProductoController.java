96dc04cccdf63d803c5924b5f8be0fad08ab4f74
MarcoRoG <urielrosasgarm@gmail.com>
2026-04-06 20:25:19 -0600

    Hicimos una versión configurada de nuestro proyecto APIREST CRUD con POSTGRESS para poder desplegar en Railway

---
+++ src/main/java/com/marcocode/apirest/apirest/Controllers/ProductoController.java
@@ -0,0 +1,68 @@
+package com.marcocode.apirest.apirest.Controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.marcocode.apirest.apirest.Repositories.ProductoRepository;
+
+
+import com.marcocode.apirest.apirest.Entities.Producto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+@RestController
+@RequestMapping("/productos")
+public class ProductoController {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @GetMapping
+    public List<Producto> obtenerAllProductos() {
+        return productoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Producto obtenerProductoPorId(@PathVariable Long id){
+        return productoRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("No se encontro el producto con el ID: " + id));
+
+    }
+    
+    
+    @PostMapping
+    public Producto crearProducto(@RequestBody Producto producto){
+        return productoRepository.save(producto);
+    }
+
+    @PutMapping("/{id}")
+    public Producto actulaizarProducto(@PathVariable Long id, @RequestBody Producto detallesProducto){
+
+        Producto producto = productoRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("No se encontro el producto con el ID: " + id));
+
+        producto.setNombre(detallesProducto.getNombre());
+        producto.setPrecio(detallesProducto.getPrecio());
+
+        return productoRepository.save(producto);
+    }
+
+    @DeleteMapping("/{id}")
+    public String eliminarProducto (@PathVariable Long id){
+         Producto producto = productoRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("No se encontro el producto con el ID: " + id));
+
+        productoRepository.delete(producto);
+        return "El producto con el ID: " + id + " fue eliminado correctamente";
+    }
+    
+}
---
+++ src/main/java/com/marcocode/apirest/apirest/Repositories/ProductoRepository.java
@@ -0,0 +1,8 @@
+package com.marcocode.apirest.apirest.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.marcocode.apirest.apirest.Entities.Producto;
+
+public interface ProductoRepository extends JpaRepository <Producto, Long> {
+
+}
